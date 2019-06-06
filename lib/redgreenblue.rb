13e8a88657d72ddf9a833581bbcee11ae379aeef
lllisteu <lllisteu@users.noreply.github.com>
2019-06-06 14:14:04 +0200

    Add hex methods. Bump version to 0.3.0.

--- lib/redgreenblue.rb
+++ lib/redgreenblue.rb
@@ -2,6 +2,9 @@ class RGB
 end
 
 require 'redgreenblue/version'
+
 require 'redgreenblue/base'
+
 require 'redgreenblue/24bit'
 require 'redgreenblue/48bit'
+require 'redgreenblue/hex'
--- lib/redgreenblue/version.rb
+++ lib/redgreenblue/version.rb
@@ -1,3 +1,3 @@
 class RGB
-	VERSION = '0.2.0'
+	VERSION = '0.3.0'
 end
