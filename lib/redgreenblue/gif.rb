628c76c57d092a68dbdc207897da1774687badf8
lllisteu <lllisteu@users.noreply.github.com>
2019-06-11 09:42:34 +0200

    gif_pixel methods

--- lib/redgreenblue/gif.rb
+++ lib/redgreenblue/gif.rb
@@ -1,3 +1,17 @@
 class RGB
 
+  # Return a 1-pixel GIF
+  #
+  # With help from:
+  # - http://www.perlmonks.org/?node_id=7974
+  def gif_pixel
+    "GIF89a\1\0\1\0\x90\0\0".b +
+    rgb.pack('C3') +
+    "\0\0\0,\0\0\0\0\1\0\1\0\0\x02\x02\x04\1\0;".b
+  end
+
+  def gif_pixel_write(file_path)
+    File.binwrite(file_path, gif_pixel)
+  end
+
 end
--- test/tests/test_gif.rb
+++ test/tests/test_gif.rb
@@ -4,8 +4,9 @@ require 'redgreenblue'
 
 class Test_gif < Test::Unit::TestCase
 
-	def test_gif_pixel
-
-	end
+  def test_gif_pixel
+    assert_equal 35, RGB.rgb([0,255,128]).gif_pixel.bytesize
+    assert_equal 'ASCII-8BIT', RGB.new.gif_pixel.encoding.name
+  end
 
 end
