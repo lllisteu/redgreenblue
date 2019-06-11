7903262c8e2995079fabd4cd437b45bd87c4ee06
lllisteu <lllisteu@users.noreply.github.com>
2019-06-11 10:04:46 +0200

    BGR 24-bit methods. Bump version to 0.5.0.

--- lib/redgreenblue/bgr24bit.rb
+++ lib/redgreenblue/bgr24bit.rb
@@ -1,3 +1,20 @@
 class RGB
 
+# bgr 24-bit methods (as used by BMP bitmaps)
+
+  def bgr24
+    [b, g, r].pack('C3')
+  end
+
+  def bgr24=(s)
+    self.b, self.g, self.r = s.unpack('C3')
+  end
+
+  # factory method
+  def self.bgr24(bgr)
+    c = self.new
+    c.bgr24 = bgr
+    c
+  end
+
 end
--- test/tests/test_bgr24bit.rb
+++ test/tests/test_bgr24bit.rb
@@ -4,8 +4,16 @@ require 'redgreenblue'
 
 class Test_bgr24bit < Test::Unit::TestCase
 
-	def test_bgr
+  def test_bgr
+    bgr = RGB.rgb([0,255,128]).bgr24
+    assert_equal 3, bgr.bytesize
 
-	end
+    c = RGB.new
+    c.bgr24 = bgr
+    assert_equal [0,255,128], c.rgb
+
+    assert_equal [0,255,128], RGB.bgr24(bgr).rgb
+
+  end
 
 end
