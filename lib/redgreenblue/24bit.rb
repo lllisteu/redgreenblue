59310fde5fe02f7e0c7fc45d20fd7f62f39c8d11
lllisteu <lllisteu@users.noreply.github.com>
2019-06-12 09:58:44 +0200

    Refactor

--- lib/redgreenblue/24bit.rb
+++ lib/redgreenblue/24bit.rb
@@ -2,48 +2,46 @@ class RGB
 
 # r, g, b methods
 
-	def r
-		(red   * 255).round
-	end
+  def r
+    (red   * 255).round
+  end
 
-	def g
-		(green * 255).round
-	end
+  def g
+    (green * 255).round
+  end
 
-	def b
-		(blue  * 255).round
-	end
+  def b
+    (blue  * 255).round
+  end
 
-	def r=(n)
-		self.red   = n / 255.0
-	end
+  def r=(n)
+    self.red   = n / 255.0
+  end
 
-	def g=(n)
-		self.green = n / 255.0
-	end
+  def g=(n)
+    self.green = n / 255.0
+  end
 
-	def b=(n)
-		self.blue  = n / 255.0
-	end
+  def b=(n)
+    self.blue  = n / 255.0
+  end
 
 # rgb methods
 
-	def rgb
-		[r,g,b]
-	end
+  def rgb
+    [r,g,b]
+  end
 
-	def rgb=(rgb)
-		self.r = rgb[0]
-		self.g = rgb[1]
-		self.b = rgb[2]
-	end
+  def rgb=(rgb)
+    self.r, self.g, self.b = rgb
+  end
 
 # factory method
 
-	def self.rgb(rgb)
-		c = self.new
-		c.rgb = rgb
-		c
-	end
+  def self.rgb(rgb)
+    c = self.new
+    c.rgb = rgb
+    c
+  end
 
 end
--- lib/redgreenblue/48bit.rb
+++ lib/redgreenblue/48bit.rb
@@ -2,48 +2,46 @@ class RGB
 
 # rr, gg, bb methods
 
-	def rr
-		(red   * 65535).round
-	end
+  def rr
+    (red   * 65535).round
+  end
 
-	def gg
-		(green * 65535).round
-	end
+  def gg
+    (green * 65535).round
+  end
 
-	def bb
-		(blue  * 65535).round
-	end
+  def bb
+    (blue  * 65535).round
+  end
 
-	def rr=(n)
-		self.red   = n / 65535.0
-	end
+  def rr=(n)
+    self.red   = n / 65535.0
+  end
 
-	def gg=(n)
-		self.green = n / 65535.0
-	end
+  def gg=(n)
+    self.green = n / 65535.0
+  end
 
-	def bb=(n)
-		self.blue  = n / 65535.0
-	end
+  def bb=(n)
+    self.blue  = n / 65535.0
+  end
 
 # rrggbb methods
 
-	def rrggbb
-		[rr,gg,bb]
-	end
+  def rrggbb
+    [rr,gg,bb]
+  end
 
-	def rrggbb=(rrggbb)
-		self.rr = rrggbb[0]
-		self.gg = rrggbb[1]
-		self.bb = rrggbb[2]
-	end
+  def rrggbb=(rrggbb)
+    self.rr, self.gg, self.bb = rrggbb
+  end
 
 # factory method
 
-	def self.rrggbb(rrggbb)
-		c = self.new
-		c.rrggbb = rrggbb
-		c
-	end
+  def self.rrggbb(rrggbb)
+    c = self.new
+    c.rrggbb = rrggbb
+    c
+  end
 
 end
