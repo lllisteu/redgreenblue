f777db0fa6fd74a874b3814f38f005f7c8ba461a
lllisteu <lllisteu@users.noreply.github.com>
2019-06-13 10:57:49 +0200

    Refactor

--- lib/redgreenblue/base.rb
+++ lib/redgreenblue/base.rb
@@ -1,30 +1,28 @@
 class RGB
 
-	attr_reader :red, :green, :blue
+  attr_reader :red, :green, :blue
 
-	def initialize(v=[0.5, 0.5, 0.5])
-		self.red   = v[0]
-		self.green = v[1]
-		self.blue  = v[2]
-	end
+  def initialize(v=[0.5, 0.5, 0.5])
+    self.red, self.green, self.blue = v
+  end
 
-	def red=(n)
-		@red = normalize(n)
-	end
+  def red=(n)
+    @red = limit(n)
+  end
 
-	def green=(n)
-		@green = normalize(n)
-	end
+  def green=(n)
+    @green = limit(n)
+  end
 
-	def blue=(n)
-		@blue = normalize(n)
-	end
+  def blue=(n)
+    @blue = limit(n)
+  end
 
-	private
+  private
 
-	# limit to 0..1
-	def normalize(n)
-		n <= 0 ? 0.0 : n >= 1 ? 1.0 : n
-	end
+  # limit to 0..1
+  def limit(n)
+    n <= 0 ? 0.0 : n >= 1 ? 1.0 : n
+  end
 
 end
