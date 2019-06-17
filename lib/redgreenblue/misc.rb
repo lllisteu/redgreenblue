28137af0c63055dc1346c97b6da13a05b04c49c2
lllisteu <lllisteu@users.noreply.github.com>
2019-06-17 14:15:49 +0200

    Add #mix!, #whiten!, #whiten, #blacken!, #blacken; Refactor. Bump version to 0.6.0.

--- lib/redgreenblue/misc.rb
+++ lib/redgreenblue/misc.rb
@@ -1,21 +1,25 @@
 class RGB
 
+# Mix
+
   # Mix with second RGB color.
   # p denotes portion of the mixed-in color to be used.
   # p=0 delivers pure original color (no change),
   # p=1 delivers pure mixed-in color.
-  def mix(color,p=0.5)
-    raise(ArgumentError, "Portion '#{p}' not in range (0..1)") unless (0..1).include? p
 
-    RGB.new([
-      ( red   * (1-p) ) + ( color.red   * p ),
-      ( green * (1-p) ) + ( color.green * p ),
-      ( blue  * (1-p) ) + ( color.blue  * p )
-    ])
+  def mix!(color,p=0.5)
+    self.values = mix_values(color.values, p)
+    self
   end
 
+  def mix(color,p=0.5)
+    RGB.new mix_values(color.values, p)
+  end
+
+# Invert
+
   def invert!
-    self.red, self.green, self.blue = 1-red, 1-green, 1-blue
+    self.values = values.map { |v| 1-v }
     self
   end
 
@@ -23,4 +27,34 @@ class RGB
     dup.invert!
   end
 
+# Mix with white
+
+  def whiten!(p)
+    mix!(RGB.white, p)
+  end
+
+  def whiten(p)
+    mix(RGB.white, p)
+  end
+
+# Mix with black
+
+  def blacken!(p)
+    mix!(RGB.black, p)
+  end
+
+  def blacken(p)
+    mix(RGB.black, p)
+  end
+
+  private
+
+  def mix_values(v, p)
+    raise(ArgumentError, "Portion '#{p}' not in range (0..1)") unless (0..1).include? p
+    [
+      ( red   * (1-p) ) + ( v[0] * p ),
+      ( green * (1-p) ) + ( v[1] * p ),
+      ( blue  * (1-p) ) + ( v[2] * p )
+    ]
+  end
 end
--- test/tests/test_misc.rb
+++ test/tests/test_misc.rb
@@ -8,13 +8,21 @@ class Test_misc < Test::Unit::TestCase
     assert_equal [128, 128, 128], RGB.white.mix(RGB.black   ).rgb
     assert_equal RGB.white.rgb,   RGB.white.mix(RGB.black, 0).rgb
     assert_equal RGB.black.rgb,   RGB.white.mix(RGB.black, 1).rgb
+
+    c = RGB.new([0,1,0.5])
+    assert_equal [128, 255, 191], c.mix(RGB.white).rgb
+    assert_equal [0,1,0.5], c.values
   end
 
-  def test_invert!
+  def test_mix!
+    assert_equal [128, 128, 128], RGB.white.mix!(RGB.black   ).rgb
+    assert_equal RGB.white.rgb,   RGB.white.mix!(RGB.black, 0).rgb
+    assert_equal RGB.black.rgb,   RGB.white.mix!(RGB.black, 1).rgb
+  end
+
+  def test_invert
     color = RGB.new([0, 0.5, 1])
-    assert_equal 1  , color.invert.red
-    assert_equal 0.5, color.invert.green
-    assert_equal 0  , color.invert.blue
+    assert_equal [1, 0.5, 0]  , color.invert.values
 
     assert_equal color.rgb   , color.invert.invert.rgb
     assert_equal color.rrggbb, color.invert.invert.rrggbb
@@ -24,4 +32,27 @@ class Test_misc < Test::Unit::TestCase
     assert_equal color.rrggbb, color.invert!.invert!.rrggbb
     assert_equal color.hex   , color.invert!.invert!.hex
   end
+
+  def test_whiten
+    assert_equal RGB.black.values, RGB.black.whiten(0).values
+    assert_equal RGB.white.values, RGB.black.whiten(1).values
+    assert_equal [0.1, 0.1, 0.1], RGB.black.whiten(0.1).values
+    assert_equal [0.75, 0.75, 0.75], RGB.black.whiten(0.75).values
+
+    c = RGB.new
+    c.whiten! 0.5
+    assert_equal [0.75, 0.75, 0.75], c.values
+  end
+
+  def test_blacken
+    assert_equal RGB.white.values, RGB.white.blacken(0).values
+    assert_equal RGB.black.values, RGB.white.blacken(1).values
+    assert_equal [0.9, 0.9, 0.9], RGB.white.blacken(0.1).values
+    assert_equal [0.25, 0.25, 0.25], RGB.white.blacken(0.75).values
+
+    c = RGB.new
+    c.blacken! 0.5
+    assert_equal [0.25, 0.25, 0.25], c.values
+  end
+
 end
