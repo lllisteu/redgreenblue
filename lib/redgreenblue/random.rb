afe2e3a1b43ba3e16922652d36e79ef3e3ac3ddf
lllisteu <lllisteu@users.noreply.github.com>
2019-09-07 09:17:48 +0200

    Refactor

--- lib/redgreenblue/random.rb
+++ lib/redgreenblue/random.rb
@@ -1,17 +1,17 @@
 class RGB
 
   def shuffle!
-    self.red, self.green, self.blue = [red, green, blue].shuffle
+    self.values = values.shuffle
     self
   end
 
   def randomize!
-    self.red, self.green, self.blue = Kernel::rand, Kernel::rand, Kernel::rand
+    self.values = Kernel::rand, Kernel::rand, Kernel::rand
     self
   end
 
   def self.rand
-    new([Kernel::rand, Kernel::rand, Kernel::rand])
+    new(Kernel::rand, Kernel::rand, Kernel::rand)
   end
 
 end
--- test/tests/test_random.rb
+++ test/tests/test_random.rb
@@ -6,12 +6,12 @@ class Test_random < Test::Unit::TestCase
 
   def test_shuffle
     10.times do
-      assert_equal [0,128,255], RGB.rgb([128,255,0]).shuffle!.rgb.sort
+      assert_equal [0,128,255], RGB.rgb(128,255,0).shuffle!.rgb.sort
     end
   end
 
   def test_randomize
-    assert_equal RGB, RGB.rgb([128,255,0]).randomize!.class
+    assert_equal RGB, RGB.rgb(128,255,0).randomize!.class
   end
 
   def test_rand
