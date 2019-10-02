63d1e56882563c5f7aed2b327335ee6567eeb65d
lllisteu <lllisteu@users.noreply.github.com>
2019-10-02 15:30:03 +0200

    Add #hue method. Bump version to 0.6.3.

--- lib/redgreenblue/hsl.rb
+++ lib/redgreenblue/hsl.rb
@@ -1,5 +1,21 @@
 class RGB
 
+  # https://en.wikipedia.org/wiki/HSL_and_HSV#Hue_and_chroma
+  def hue
+    if chroma == 0.0
+      nil
+    else
+      case to_h.keys.first
+      when :red
+        60 * ( ( ( green - blue  ) / chroma ).modulo 6 )
+      when :green
+        60 * ( (   blue  - red   ) / chroma + 2 )
+      when :blue
+        60 * ( (   red   - green ) / chroma + 4 )
+      end
+    end
+  end
+
   # Lightness as the average of the min and max of RGB values.
   # https://en.wikipedia.org/wiki/HSL_and_HSV#Lightness
   def lightness
--- test/tests/test_hsl.rb
+++ test/tests/test_hsl.rb
@@ -10,8 +10,21 @@ class Test_hsl < Test::Unit::TestCase
     ['#808080', 0.502],
     ['#000000', 0.0  ],
     ['#FF0000', 0.5  ],
-    ].each do |h,l|
-      assert_equal l, RGB.hex(h).lightness.round(3)
+    ].each do |hx,l|
+      assert_equal l, RGB.hex(hx).lightness.round(3)
+    end
+  end
+
+  def test_hue
+    assert_nil RGB.white.hue
+    assert_nil RGB.grey.hue
+    assert_nil RGB.black.hue
+    [
+    ['#FF0000', 0.0],
+    ['#00FF00', 120.0],
+    ['#0000FF', 240.0]
+    ].each do |hx,h|
+      assert_equal h, RGB.hex(hx).hue
     end
   end
 
