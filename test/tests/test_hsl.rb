620b1771bca095585086cb061d4276f24d446b95
lllisteu <lllisteu@users.noreply.github.com>
2019-10-04 14:28:00 +0200

    Order tests

--- test/tests/test_hsl.rb
+++ test/tests/test_hsl.rb
@@ -4,17 +4,6 @@ require 'redgreenblue'
 
 class Test_hsl < Test::Unit::TestCase
 
-  def test_lightness
-    [
-    ['#FFFFFF', 1.0  ],
-    ['#808080', 0.502],
-    ['#000000', 0.0  ],
-    ['#FF0000', 0.5  ],
-    ].each do |hx,l|
-      assert_equal l, RGB.hex(hx).lightness.round(3)
-    end
-  end
-
   def test_hue
     assert_nil RGB.white.hue
     assert_nil RGB.grey.hue
@@ -25,6 +14,17 @@ class Test_hsl < Test::Unit::TestCase
     ['#0000FF', 240.0]
     ].each do |hx,h|
       assert_equal h, RGB.hex(hx).hue
+    end
+  end
+
+  def test_lightness
+    [
+    ['#FFFFFF', 1.0  ],
+    ['#808080', 0.502],
+    ['#000000', 0.0  ],
+    ['#FF0000', 0.5  ],
+    ].each do |hx,l|
+      assert_equal l, RGB.hex(hx).lightness.round(3)
     end
   end
 
