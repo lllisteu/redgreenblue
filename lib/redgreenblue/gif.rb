01207abd1b0edba8c269790e2d1c7e5cd8098265
lllisteu <lllisteu@users.noreply.github.com>
2019-10-07 16:28:54 +0200

    Comments & docs

--- lib/redgreenblue/gif.rb
+++ lib/redgreenblue/gif.rb
@@ -1,15 +1,16 @@
 class RGB
 
-  # Return a 1-pixel GIF
+  # Returns a 1-pixel GIF image set to the color.
   #
   # With help from:
-  # - http://www.perlmonks.org/?node_id=7974
+  # - https://www.perlmonks.org/?node_id=7974
   def gif_pixel
     "GIF89a\1\0\1\0\x90\0\0".b +
     rgb.pack('C3') +
     "\0\0\0,\0\0\0\0\1\0\1\0\0\x02\x02\x04\1\0;".b
   end
 
+  # Writes a 1-pixel GIF image to a file.
   def gif_pixel_write(file_path)
     File.binwrite(file_path, gif_pixel)
   end
