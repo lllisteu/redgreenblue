aa7ec2853255000e3895f9b4944b930fcfdd8c98
lllisteu <lllisteu@users.noreply.github.com>
2019-10-28 15:38:31 +0100

    Optional support for Philips Hue API

---
+++ lib/redgreenblue/opt/philipshue.rb
@@ -0,0 +1,36 @@
+class RGB
+
+  # Return the arguments required by the Philips Hue API
+  # to set a light to this color.
+  #
+  # When formatted as JSON, this hash can be used
+  # directly to set a light's state.
+  def to_philips_hue_api_hsb_arguments(black_off=true)
+    my_hsb = hsb
+
+    # Black means 'off'
+    if black_off and ( my_hsb[2] == 0 )
+      { 'on'  => false }
+
+    else
+      {
+
+        # On/Off state of the light
+        'on'  => true,
+
+        # Brightness 1..254
+        # Note: a brightness of 1 will not switch the light off
+        'bri' => (  my_hsb[2]        * 253 + 1     ).round,
+
+        # Hue 0..65535
+        'hue' => (( my_hsb[0] || 0 ) * 65535 / 360 ).round,
+
+        # Saturation 0..254
+        'sat' => (  my_hsb[1]        * 254         ).round
+
+      }
+
+    end
+  end
+
+end
---
+++ test/tests/test_opt_philipshue.rb
@@ -0,0 +1,23 @@
+require 'test/unit'
+
+require 'redgreenblue'
+require 'redgreenblue/opt/philipshue'
+
+class Test_opt_philipshue < Test::Unit::TestCase
+
+  def test_simple
+
+    assert_equal %w(on bri hue sat), RGB.new.to_philips_hue_api_hsb_arguments.keys
+
+    assert_equal ({ 'on' => false }), RGB.black.to_philips_hue_api_hsb_arguments
+
+    assert_equal     0, RGB.new(1,0,0).to_philips_hue_api_hsb_arguments['hue']
+    assert_equal 21845, RGB.new(0,1,0).to_philips_hue_api_hsb_arguments['hue']
+    assert_equal 43690, RGB.new(0,0,1).to_philips_hue_api_hsb_arguments['hue']
+
+    assert_equal   1, RGB.black.to_philips_hue_api_hsb_arguments(false)['bri']
+    assert_equal 254, RGB.white.to_philips_hue_api_hsb_arguments['bri']
+
+  end
+
+end
