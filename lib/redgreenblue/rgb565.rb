7eb5ddd25cc330a80ed84d0c0643331b2fe51a33
lllisteu <lllisteu@users.noreply.github.com>
2019-11-01 14:06:23 +0100

    Comments & readability

--- lib/redgreenblue/rgb565.rb
+++ lib/redgreenblue/rgb565.rb
@@ -1,25 +1,29 @@
 class RGB
 
+  # Returns the color in 16-bit RGB565 format.
   def rgb565
     [((r >> 3) << 11) + ((g >> 2) << 5) + (b >> 3)].pack 'S<'
   end
 
-  # https://stackoverflow.com/questions/2442576/
-  def rgb565=(s)
-    v = ( s.unpack "S<" )[0]
+  # Sets the color from 16-bit RGB565 data.
+  # With help from:
+  # - https://stackoverflow.com/questions/2442576/
+  def rgb565=(rgb565_string)
+    v = ( rgb565_string.unpack "S<" )[0]
     self.r = ( ( v & 0xf800 ) >> 11 ) << 3
     self.g = ( ( v & 0x07e0 ) >>  5 ) << 2
     self.b = ( ( v & 0x001f )       ) << 3
   end
 
+  # Returns the color in 16-bit RGB565 format as a string of 0's and 1's
   def rgb565_binary
     rgb565.bytes.reverse.map { |b| "%08b" % b }.join
   end
 
-  # factory method
-  def self.rgb565(s)
+  # Creates a new RGB color from 16-bit RGB565 data.
+  def self.rgb565(rgb565_string)
     c = self.new
-    c.rgb565 = s
+    c.rgb565 = rgb565_string
     c
   end
 
