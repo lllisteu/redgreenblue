1f28079b339a84e487b79a7ba62bb4305a422d1c
lllisteu <lllisteu@users.noreply.github.com>
2019-11-03 16:40:25 +0100

    Add ::red, ::green, ::blue factory methods. Comments, readability.

--- lib/redgreenblue/lazy.rb
+++ lib/redgreenblue/lazy.rb
@@ -1,18 +1,38 @@
 class RGB
 
+  # Creates a white RGB object.
   def self.white
     new(1,1,1)
   end
 
+  # Creates a black RGB object.
   def self.black
     new(0,0,0)
   end
 
-  def self.grey(n=0.5)
-    new(n,n,n)
+  # Creates a grey RGB object. Defaults to lightness 0.5, a middle grey. Black equals grey(0), white equals grey(1).
+  #
+  # ::gray is an alias for ::grey.
+  def self.grey(lightness=0.5)
+    new(lightness, lightness, lightness)
   end
 
-  # Alias gray for grey
+  # Alias gray for grey.
   self.singleton_class.send(:alias_method, :gray, :grey)
+
+  # Creates a pure red RGB object.
+  def self.red
+    new(1,0,0)
+  end
+
+  # Creates a pure green RGB object.
+  def self.green
+    new(0,1,0)
+  end
+
+  # Creates a pure blue RGB object.
+  def self.blue
+    new(0,0,1)
+  end
 
 end
--- test/tests/test_lazy.rb
+++ test/tests/test_lazy.rb
@@ -9,6 +9,10 @@ class Test_lazy < Test::Unit::TestCase
     assert_equal [  0,   0,   0], RGB.black.rgb
     assert_equal [ 85,  85,  85], RGB.grey(1.0/3).rgb
     assert_equal [ 51,  51,  51], RGB.gray(1.0/5).rgb
+
+    assert_equal [255,   0,   0], RGB.red.rgb
+    assert_equal [  0, 255,   0], RGB.green.rgb
+    assert_equal [  0,   0, 255], RGB.blue.rgb
   end
 
 end
