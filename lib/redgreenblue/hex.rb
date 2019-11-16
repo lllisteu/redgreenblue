c70e3553212dc04fe68cc2a8112bc3c70a6c3e4d
lllisteu <lllisteu@users.noreply.github.com>
2019-11-16 11:43:10 +0100

    Refactor (argument checking, comments, docs, readability)

--- lib/redgreenblue/hex.rb
+++ lib/redgreenblue/hex.rb
@@ -1,30 +1,47 @@
 class RGB
 
+  # Returns a 6-digit hexadecimal string representing the object's red, green, and blue components as 8-bit values.
+  #
+  # If shorthand is true, returns 3-digit shorthand if possible.
   def hex(shorthand=false)
     if shorthand
-      RGB.hex_shorthand hexadecimal
+      RGB.hex_shorthand hex6
     else
-      hexadecimal
+      hex6
     end
   end
 
-  def hex=(s)
-    if ( s =~ /^(#?)(\h\h)(\h\h)(\h\h)$/ ) # 6-digit hex
-      self.rgb = [ $2.to_i(16), $3.to_i(16), $4.to_i(16) ]
-    elsif  ( s =~ /^(#?)(\h)(\h)(\h)$/ ) # 3-digit hex
-      self.rgb = [ ($2*2).to_i(16), ($3*2).to_i(16), ($4*2).to_i(16) ]
+  # Sets red, green, and blue using a 6- or 3-digit hexadecimal string.
+  #
+  # The string may include a '#' prefix.
+  def hex=(hex_string)
+    if rgb = RGB.hex_to_rgb(hex_string)
+      self.rgb = rgb
+    else
+      raise ArgumentError, "'#{hex_string}' is not a usable hexadecimal string"
     end
   end
 
-  # factory method
-  def self.hex(s)
-    c = self.new
-    c.hex = s
-    c
+
+# Class methods
+
+
+  # Creates a new object from a 6- or 3-digit hexadecimal string representing  red, green, and blue.
+  #
+  # The string may include a '#' prefix.
+  def self.hex(hex_string)
+    if rgb = RGB.hex_to_rgb(hex_string)
+      self.rgb rgb
+    else
+      raise ArgumentError, "'#{hex_string}' is not a usable hexadecimal string"
+    end
   end
 
-  def self.hex_shorthand(h)
-    h.sub( /^(#?)(\h)\2(\h)\3(\h)\4$/, '\1\2\3\4' )
+  # Returns a 3-digit shorthand version of a 6-digit hexadecimal string.
+  #
+  # If a shorthand version is not possible, returns the original string.
+  def self.hex_shorthand(hex_string)
+    hex_string.sub( /^(#?)(\h)\2(\h)\3(\h)\4$/, '\1\2\3\4' )
   end
 
   # Parses a 6- or 3-digit hexadecimal string.
@@ -32,24 +49,18 @@ class RGB
   #
   # The string may include a '#' prefix.
   def self.hex_to_rgb(hex_string)
-
-    # 6-digit hex
     if hex_string =~ /^(#?)(\h\h)(\h\h)(\h\h)$/
       [$2, $3, $4].map { |h| h.to_i(16) }
-
-    # 3-digit hex
     elsif hex_string =~ /^(#?)(\h)(\h)(\h)$/
       [$2, $3, $4].map { |h| (h*2).to_i(16) }
-
     else
       nil
-
     end
   end
 
   private
 
-  def hexadecimal
+  def hex6
     '%02x%02x%02x' % [ r, g, b ]
   end
 
