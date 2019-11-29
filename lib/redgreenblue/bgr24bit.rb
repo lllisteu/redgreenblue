799412a8d4b9dbf868fb39534585c8b602b3460e
lllisteu <lllisteu@users.noreply.github.com>
2019-11-29 10:53:12 +0100

    Comments & docs

--- lib/redgreenblue/bgr24bit.rb
+++ lib/redgreenblue/bgr24bit.rb
@@ -1,16 +1,16 @@
 class RGB
 
-# bgr 24-bit methods (as used by BMP bitmaps)
-
+  # Returns a 3-byte string containing the object's color in BGR24 format.
   def bgr24
     [b, g, r].pack('C3')
   end
 
-  def bgr24=(s)
-    self.b, self.g, self.r = s.unpack('C3')
+  # Sets red, green, and blue using BGR24 data (a 3-byte string).
+  def bgr24=(bgr_string)
+    self.b, self.g, self.r = bgr_string.unpack('C3')
   end
 
-  # factory method
+  # Creates a new RGB object from BGR24 data (a 3-byte string).
   def self.bgr24(bgr)
     c = self.new
     c.bgr24 = bgr
