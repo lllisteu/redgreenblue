c19c4a33f38f94e03ec14d6dc3e28934de61a75c
lllisteu <lllisteu@users.noreply.github.com>
2019-12-08 18:16:01 +0100

    Refine tests

--- test/tests/test_cie.rb
+++ test/tests/test_cie.rb
@@ -4,14 +4,30 @@ require 'redgreenblue'
 
 class Test_cie < Test::Unit::TestCase
 
-  def test_simple
-  end
-
   # https://ninedegreesbelow.com/photography/xyz-rgb.html
   # "If you add up the Y values for the Red, Blue, and Green primaries...the sum is always 1.0000".
   def test_case1
-    assert_equal 1.000000, (RGB.red.xyz[1] + RGB.green.xyz[1] + RGB.blue.xyz[1]).round(6)
+    assert_equal 1.0000, (RGB.red.cie_xyz[1] + RGB.green.cie_xyz[1] + RGB.blue.cie_xyz[1])
+    assert_equal 1.0000, (RGB.red.cie_xyy[2] + RGB.green.cie_xyy[2] + RGB.blue.cie_xyy[2])
+  end
+
+  # Test x,y values for RGB primaries from:
+  # http://www.brucelindbloom.com
+  def test_case2
+    assert_equal [ 0.6400, 0.3300 ], RGB.red.cie_xy
+    assert_equal [ 0.3000, 0.6000 ], RGB.green.cie_xy
+    assert_equal [ 0.1500, 0.0600 ], RGB.blue.cie_xy
+  end
+
+  def test_black
+    assert_true RGB.black.xy[0].nan?
+    assert_true RGB.black.xy[1].nan?
+  end
+
+  def test_aliases
+    assert_equal RGB.white.cie_xyz, RGB.white.xyz
+    assert_equal RGB.white.cie_xyy, RGB.white.xyy
+    assert_equal RGB.white.cie_xy, RGB.white.xy
   end
 
 end
-
