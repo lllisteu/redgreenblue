2e3a88fb87a20e3ae62c25bab99bcfd6b6b358b2
lllisteu <lllisteu@users.noreply.github.com>
2019-12-16 10:57:45 +0100

    Test inspect styles

--- test/tests/test_inspect.rb
+++ test/tests/test_inspect.rb
@@ -4,9 +4,16 @@ require 'redgreenblue'
 
 class Test_inspect < Test::Unit::TestCase
 
-  def test_simple
-    assert_equal String, RGB.new.inspect.class
+  def test_to_s
     assert_equal String, RGB.new.to_s.class
+  end
+
+  def test_inspect
+    RGB.styles.each do |s|
+      RGB.style = s
+      assert_equal s, RGB.style
+      assert_equal String, RGB.new.inspect.class
+    end
   end
 
 end
