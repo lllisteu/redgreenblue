7543a995a3c6eddb00d9ecbcec48ea8b4f663126
lllisteu <lllisteu@users.noreply.github.com>
2020-02-06 12:27:05 +0100

    Add ::at method

--- lib/redgreenblue/int.rb
+++ lib/redgreenblue/int.rb
@@ -5,4 +5,13 @@ class RGB
     ( r << 16 ) + ( g << 8 ) + b
   end
 
+  # Creates a new RGB object from a 24-bit integer in the range 0..16777215.
+  def self.at(number)
+    if (0..16777215) === number.to_i
+      RGB.hex( '%06x' % number.to_i )
+    else
+      raise ArgumentError, "Argument '#{number}' not in range 0..16777215"
+    end
+  end
+
 end
--- test/tests/test_int.rb
+++ test/tests/test_int.rb
@@ -12,4 +12,14 @@ class Test_int < Test::Unit::TestCase
     assert_equal  4563356, RGB.rgb(69,161,156).to_i
   end
 
+  def test_at
+    assert_equal RGB.black, RGB.at(       0)
+    assert_equal RGB.white, RGB.at(16777215)
+
+    assert_equal RGB.black, RGB.at(0.9)
+
+    # https://codegolf.stackexchange.com/questions/43155
+    assert_equal RGB.rgb(69,161,156), RGB.at(4563356)
+  end
+
 end
