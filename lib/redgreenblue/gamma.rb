9ac3addc10136b73f6941d6d8a3203547dce9a1e
lllisteu <lllisteu@users.noreply.github.com>
2020-02-15 10:29:34 +0100

    Add #linear_values= method

--- lib/redgreenblue/gamma.rb
+++ lib/redgreenblue/gamma.rb
@@ -16,7 +16,28 @@ class RGB
         end
       }
     else
-      raise "Can not compute gamma for color space #{color_space}"
+      raise "can not compute gamma for color space '#{color_space}'"
+    end
+  end
+
+  # Sets the object's RGB values using three linear RGB values, each between 0 and 1.
+  # Linear values will be converted (gamma-companded) to the object's gamma.
+  #
+  # Based on:
+  # - https://en.wikipedia.org/wiki/SRGB
+  # - http://www.brucelindbloom.com/Eqn_XYZ_to_RGB.html
+  # - https://entropymine.com/imageworsener/srgbformula/
+  def linear_values=(*a)
+    if color_space == 'sRGB'
+      self.values = a.flatten.map { |v|
+        if v <= 0.0031308
+          v * 12.92
+        else
+          1.055 * ( v ** ( 1/2.4 ) ) - 0.055
+        end
+      }.map { |v| v.round(9) }
+    else
+      raise "can not compute gamma for color space '#{color_space}'"
     end
   end
 
--- test/tests/test_gamma.rb
+++ test/tests/test_gamma.rb
@@ -4,12 +4,26 @@ require 'redgreenblue'
 
 class Test_gamma < Test::Unit::TestCase
 
-  def test_linear
+  def test_linear_get
     assert_equal [1.0, 1.0, 1.0], RGB.white.linear_values
     assert_equal [0.0, 0.0, 0.0], RGB.black.linear_values
 
     # https://entropymine.com/imageworsener/srgbformula/
     assert_equal [0.00313080, 0.00313080, 0.00313080], RGB.grey(0.04045).linear_values.map { |v| v.round(8) }
+  end
+
+  def test_linear_set
+    c = RGB.grey
+
+    c.linear_values = [1, 1, 1]
+    assert_equal [1, 1, 1], c.values
+
+    c.linear_values = [0, 0, 0]
+    assert_equal [0, 0, 0], c.values
+
+    # https://entropymine.com/imageworsener/srgbformula/
+    c.linear_values = [0.00313080, 0.00313080, 0.00313080]
+    assert_equal [0.04045, 0.04045, 0.04045], c.values.map { |v| v.round(6)}
   end
 
 end
