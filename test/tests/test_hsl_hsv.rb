9b221ae3c11871682e1e9196a97d4ec796f5e78b
lllisteu <lllisteu@users.noreply.github.com>
2020-03-24 14:46:38 +0100

    Refactor & expand tests

--- test/tests/test_hsl_hsv.rb
+++ test/tests/test_hsl_hsv.rb
@@ -5,49 +5,58 @@ require 'redgreenblue'
 class Test_hsl_hsv < Test::Unit::TestCase
 
   def test_to_hsl
-    assert_equal [  nil, 0.0, 1.0], RGB.hex('#FFFFFF').hsl
-    assert_equal [  nil, 0.0, 0.0], RGB.hex('#000000').hsl
-    assert_equal [  0.0, 1.0, 0.5], RGB.hex('#FF0000').hsl
-    assert_equal [120.0, 1.0, 0.5], RGB.hex('#00FF00').hsl
-    assert_equal [240.0, 1.0, 0.5], RGB.hex('#0000FF').hsl
+    assert_equal [  nil, 0.0, 0.0], RGB.new(0, 0, 0).hsl
+    assert_equal [  nil, 0.0, 0.5], RGB.new(0.5, 0.5, 0.5).hsl
+    assert_equal [  nil, 0.0, 1.0], RGB.new(1, 1, 1).hsl
+
+    assert_equal [  0.0, 1.0, 0.5], RGB.new(1, 0, 0).hsl
+    assert_equal [ 60.0, 1.0, 0.5], RGB.new(1, 1, 0).hsl
+    assert_equal [120.0, 1.0, 0.5], RGB.new(0, 1, 0).hsl
+    assert_equal [180.0, 1.0, 0.5], RGB.new(0, 1, 1).hsl
+    assert_equal [240.0, 1.0, 0.5], RGB.new(0, 0, 1).hsl
+    assert_equal [300.0, 1.0, 0.5], RGB.new(1, 0, 1).hsl
   end
 
   def test_from_hsl
-    assert_equal RGB.red,     RGB.hsl(  0,1,0.5)
-    assert_equal RGB.yellow,  RGB.hsl( 60,1,0.5)
-    assert_equal RGB.green,   RGB.hsl(120,1,0.5)
-    assert_equal RGB.cyan,    RGB.hsl(180,1,0.5)
-    assert_equal RGB.blue,    RGB.hsl(240,1,0.5)
-    assert_equal RGB.magenta, RGB.hsl(300,1,0.5)
+    assert_equal RGB.new(0, 0, 0), RGB.hsl(nil, 0, 0)
+    assert_equal RGB.new(0.5, 0.5, 0.5), RGB.hsl(nil, 0, 0.5)
+    assert_equal RGB.new(1, 1, 1), RGB.hsl(nil, 0, 1)
 
-    assert_equal RGB.white,   RGB.hsl(nil,1,1)
-    assert_equal RGB.white,   RGB.hsl(6,1,1)
+    assert_equal RGB.new(1, 1, 1), RGB.hsl(nil,1,1)
+    assert_equal RGB.new(1, 1, 1), RGB.hsl(6,1,1)
+
+    assert_equal RGB.new(1, 0, 0), RGB.hsl(  0, 1, 0.5)
+    assert_equal RGB.new(1, 1, 0), RGB.hsl( 60, 1, 0.5)
+    assert_equal RGB.new(0, 1, 0), RGB.hsl(120, 1, 0.5)
+    assert_equal RGB.new(0, 1, 1), RGB.hsl(180, 1, 0.5)
+    assert_equal RGB.new(0, 0, 1), RGB.hsl(240, 1, 0.5)
+    assert_equal RGB.new(1, 0, 1), RGB.hsl(300, 1, 0.5)
   end
 
   def test_hsv
-    assert_equal [nil, 0, 0.5], RGB.grey.hsv
-    assert_equal [nil, 0, 0  ], RGB.black.hsv
-    assert_equal [nil, 0, 1  ], RGB.white.hsv
+    assert_equal [nil, 0.0, 0.0], RGB.new(0, 0, 0).hsv
+    assert_equal [nil, 0.0, 0.5], RGB.new(0.5, 0.5, 0.5).hsv
+    assert_equal [nil, 0.0, 1.0], RGB.new(1, 1, 1).hsv
 
-    assert_equal [  0, 1, 1], RGB.new(1,0,0).hsv
-    assert_equal [ 60, 1, 1], RGB.new(1,1,0).hsv
-    assert_equal [120, 1, 1], RGB.new(0,1,0).hsv
-    assert_equal [180, 1, 1], RGB.new(0,1,1).hsv
-    assert_equal [240, 1, 1], RGB.new(0,0,1).hsv
-    assert_equal [300, 1, 1], RGB.new(1,0,1).hsv
+    assert_equal [  0.0, 1.0, 1.0], RGB.new(1, 0, 0).hsv
+    assert_equal [ 60.0, 1.0, 1.0], RGB.new(1, 1, 0).hsv
+    assert_equal [120.0, 1.0, 1.0], RGB.new(0, 1, 0).hsv
+    assert_equal [180.0, 1.0, 1.0], RGB.new(0, 1, 1).hsv
+    assert_equal [240.0, 1.0, 1.0], RGB.new(0, 0, 1).hsv
+    assert_equal [300.0, 1.0, 1.0], RGB.new(1, 0, 1).hsv
 
-    assert_equal [ 90, 1, 1  ], RGB.new(0.5,1,0).hsv
-    assert_equal [300, 1, 0.5], RGB.new(0.5,0,0.5).hsv
+    assert_equal [ 90.0, 1.0, 1.0], RGB.new(0.5, 1, 0).hsv
+    assert_equal [300.0, 1.0, 0.5], RGB.new(0.5, 0, 0.5).hsv
 
-    assert_equal [210, 2.0/3, 0.3], RGB.new(0.1,0.2,0.3).hsv
+    assert_equal [210.0, 2.0/3, 0.3], RGB.new(0.1, 0.2, 0.3).hsv
   end
 
   def test_hsb
-    assert_equal [315, 0.8, 1], RGB.hex('f3c').hsb
+    assert_equal [315.0, 0.8, 1.0], RGB.hex('f3c').hsb
   end
 
   def test_bugs
-    assert_equal 1.0, RGB.hex('f9c').hsl[1]
+    assert_equal [330.0, 1.0, 0.8], RGB.hex('f9c').hsl
   end
 
 end
