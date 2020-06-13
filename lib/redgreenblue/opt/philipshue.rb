21a213c0efb05c595876121e96e7bb6499c2eaea
lllisteu <lllisteu@users.noreply.github.com>
2020-06-13 14:02:19 +0200

    Comments & docs

--- lib/redgreenblue/opt/philipshue.rb
+++ lib/redgreenblue/opt/philipshue.rb
@@ -1,19 +1,28 @@
 # Optional support for Philips Hue lights.
 #
 # Conforms to Bridge API 1.35 for Philips Hue, published 20-Nov-2019.
-# See (regrettably requires registration):
-# - https://developers.meethue.com/develop/hue-api/
 
 # Automatically load core RGB class before loading options.
 require 'redgreenblue'
 
 class RGB
 
-  # Returns a hash with the arguments required by the Philips Hue API
-  # to set a light to this RGB object's hue, saturation and brightness (HSB).
-  # Only available when optional support for Philips Hue lights is loaded (require 'redgreenblue/opt/philipshue').
+  # Only available when optional support for Philips Hue lights is loaded.
+  #
+  # Returns a hash with the arguments required by the Philips Hue API,
+  # to set a light to this RGB object's hue, saturation and brightness.
   #
   # Formatted as JSON, this hash can be sent to a bridge to set a light's state.
+  #
+  # See (regrettably requires registration):
+  # - https://developers.meethue.com/develop/hue-api/
+  #
+  # @example Load
+  #  require 'redgreenblue/opt/philipshue'
+  # @example Use
+  #  RGB.magenta.to_philips_hue_api_hsb_arguments
+  #  => {"on"=>true, "bri"=>254, "hue"=>54613, "sat"=>254}
+  # @return [Hash] API arguments
   def to_philips_hue_api_hsb_arguments(black_off=true)
     my_hsb = hsb
 
