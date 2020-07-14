3293b73390326d0527ba3dd982d2f4457407ec9b
lllisteu <lllisteu@users.noreply.github.com>
2020-07-14 16:12:41 +0200

    Add cie_luv; refactor lab/luv code.

--- lib/redgreenblue/cie_1976.rb
+++ lib/redgreenblue/cie_1976.rb
@@ -1,16 +1,37 @@
 class RGB
 
   # Returns CIE 1976 L*a*b* (CIELAB) values for the RGB object.
+  def cie_lab(round: true)
+    cie_lab_luv(round: round, type: :lab)
+  end
+
+  alias lab cie_lab
+
+  # Returns CIE 1976 L*u*v* (CIELUV) values for the RGB object.
+  def cie_luv(round: true)
+    cie_lab_luv(round: round, type: :luv)
+  end
+
+  alias luv cie_luv
+
+  private
+
+  # Returns either CIE 1976 L*a*b* (CIELAB) or CIE 1976 L*u*v* (CIELUV) values for the RGB object.
   #
-  # Based on:
+  # L*a*b* formula based on:
   # - http://www.brucelindbloom.com/Eqn_XYZ_to_Lab.html
   # - https://en.wikipedia.org/wiki/CIELAB_color_space
   # With peeking at:
   # - https://github.com/halostatue/color/blob/master/lib/color/rgb.rb
-  def cie_lab(round: true)
-    f = cie_xyz(round: false).zip(RGB.white.cie_xyz(round: false)).map {
-      |a| a[0] / a[1]
-    }.map { |v|
+  #
+  # L*u*v* formula based on:
+  # - http://www.brucelindbloom.com/Eqn_XYZ_to_Luv.html
+  # - https://en.wikipedia.org/wiki/CIELUV
+  def cie_lab_luv(round: true, type: :lab)
+    x , y , z  =           cie_xyz(round: false)
+    xr, yr, zr = RGB.white.cie_xyz(round: false)
+
+    f = [ x / xr, y / yr, z / zr ].map { |v|
       if v > ( 216.0 / 24389 )
         v ** ( 1.0 / 3 ) # cube root
       else
@@ -18,15 +39,19 @@ class RGB
       end
     }
 
-    [
-
-      116 *  f[1] - 16,
-      500 * (f[0] - f[1]),
-      200 * (f[1] - f[2])
-
-    ].map { |v| round ? v.round(8) : v }
+    if type == :luv
+      [
+        l = 116 * f[1] - 16,
+        13 * l * ( ( 4 * x ) / ( x + 15 * y + 3 * z ) - ( 4 * xr ) / ( xr + 15 * yr + 3 * zr ) ),
+        13 * l * ( ( 9 * y ) / ( x + 15 * y + 3 * z ) - ( 9 * yr ) / ( xr + 15 * yr + 3 * zr ) )
+      ]
+    else
+      [
+        116 *  f[1] - 16,
+        500 * (f[0] - f[1]),
+        200 * (f[1] - f[2])
+      ]
+    end.map { |v| v.nan? ? 0.0 : ( round ? v.round(8) : v ) }
   end
-
-  alias lab cie_lab
 
 end
--- test/tests/test_cie_1976.rb
+++ test/tests/test_cie_1976.rb
@@ -19,8 +19,24 @@ class Test_cie_1976 < Test::Unit::TestCase
     assert_equal [ 100          ,   0          ,    0           ], RGB.new( 1  , 1  , 1   ).cie_lab
   end
 
+  # Test CIE 1976 L*u*v* values calculated with:
+  # - http://www.brucelindbloom.com/ColorCalculator.html
+  def test_luv
+    assert_equal [  53.2407_8887, 175.0151_0209,   37.7564_1210 ], RGB.new( 1  , 0  , 0   ).cie_luv
+    assert_equal [  97.1392_6343,   7.7056_4145,  106.7866_1741 ], RGB.new( 1  , 1  , 0   ).cie_luv
+    assert_equal [  87.7347_2019, -83.0775_6144,  107.3985_3299 ], RGB.new( 0  , 1  , 0   ).cie_luv
+    assert_equal [  91.1132_1748, -70.4773_4272,  -15.2042_3988 ], RGB.new( 0  , 1  , 1   ).cie_luv
+    assert_equal [  32.2970_0944,  -9.4054_0488, -130.3423_4448 ], RGB.new( 0  , 0  , 1   ).cie_luv
+    assert_equal [  60.3242_0719,  84.0714_2508, -108.6833_5039 ], RGB.new( 1  , 0  , 1   ).cie_luv
+
+    assert_equal [   0          ,   0          ,    0           ], RGB.new( 0  , 0  , 0   ).cie_luv
+    assert_equal [  53.3889_6474,   0          ,    0           ], RGB.new( 0.5, 0.5, 0.5 ).cie_luv
+    assert_equal [ 100          ,   0          ,    0           ], RGB.new( 1  , 1  , 1   ).cie_luv
+  end
+
   def test_aliases
     assert_equal RGB.white.cie_lab, RGB.white.lab
+    assert_equal RGB.white.cie_luv, RGB.white.luv
   end
 
 end
