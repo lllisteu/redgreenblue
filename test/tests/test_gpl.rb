eab6f3af4b61caab6448bf9668ffe7c610ca1e43
lllisteu <lllisteu@users.noreply.github.com>
2020-08-04 10:05:42 +0200

    #gpl picks up name.

--- test/tests/test_gpl.rb
+++ test/tests/test_gpl.rb
@@ -6,9 +6,20 @@ class Test_gpl < Test::Unit::TestCase
 
   def test_to_gpl
     assert_equal '  0   0   0', RGB.rgb(0, 0, 0).gpl
-    assert_equal "128   0 255\t", RGB.rgb(128, 0, 255).gpl('')
     assert_equal "138 153 119\tBlassgrün", RGB.rgb(138, 153, 119).gpl('Blassgrün')
     assert_equal "  0 170   0\t2", RGB.rgb(0, 170, 0).gpl(2)
+
+    c = RGB.hex 'f9c'
+    assert_equal '255 153 204', c.gpl
+    assert_equal '255 153 204', c.gpl(nil)
+    assert_equal '255 153 204', c.gpl('')
+    assert_equal "255 153 204\tpink", c.gpl('pink')
+
+    c.name = 'ピンク'
+    assert_equal "255 153 204\tピンク", c.gpl
+    assert_equal '255 153 204', c.gpl(nil)
+    assert_equal '255 153 204', c.gpl('')
+    assert_equal "255 153 204\tpink", c.gpl('pink')
   end
 
 end
