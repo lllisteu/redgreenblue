efc12d3e30941add96a2660cb90eb66e2135cb27
lllisteu <lllisteu@users.noreply.github.com>
2020-08-19 10:26:55 +0200

    Scaffolding for CSS named colors.

--- test/tests/test_web.rb
+++ test/tests/test_web.rb
@@ -4,7 +4,7 @@ require 'redgreenblue'
 
 class Test_web < Test::Unit::TestCase
 
-  def test_simple
+  def test_css_hex
     assert_equal '#fb0'   , RGB.rgb(255,187,0).css_hex
     assert_equal '#ffbc00', RGB.rgb(255,188,0).css_hex
     assert_equal '#f9c'   , RGB.new(1, 0.6, 0.8).css_hex
