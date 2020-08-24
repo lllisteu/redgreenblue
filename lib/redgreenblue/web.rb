d84f6d3b7bad6aa57caf7ddcea66d63339ec4e20
lllisteu <lllisteu@users.noreply.github.com>
2020-08-24 14:25:19 +0200

    Implement css class method.

--- lib/redgreenblue/web.rb
+++ lib/redgreenblue/web.rb
@@ -6,6 +6,49 @@ class RGB
 
   class << self
 
+    # Returns CSS named colors, as per CSS Color Module Level 4.
+    #
+    # Optional selector argument can be:
+    # - String or Regexp (to select by name)
+    # - RGB color (to select by color)
+    # - Integer (to select by index).
+    # Selection by name (string or regular expression) is case-insensitive by default.
+    #
+    # @example No Selector
+    #  # All colors
+    #  RGB.css
+    #
+    #  # Pastels
+    #  RGB.css.select { |c| c.ostwald_cwk[1] > 0.6 }
+    # @example Select by Name
+    #  # Exact name
+    #  RGB.css 'pink'
+    #
+    #  # Regular expression
+    #  RGB.css /pink|rose/
+    #
+    # @example Select by RGB color
+    #  RGB.css RGB.hex('0ff')
+    def css(selector=nil)
+      @@css ||= load_gpl file: ( File.dirname(__FILE__) + '/palettes/css.gpl' ), freeze: true
+      case selector
+        when NilClass
+          @@css
+        when String
+          n = selector.downcase
+          @@css.select { |c| c.name == n }.first
+        when Regexp
+          r = Regexp.new selector.source, Regexp::IGNORECASE
+          @@css.select { |c| c.name =~ r }
+        when Integer
+          @@css[selector]
+        when self
+          @@css.select { |c| c == selector }
+        else
+          raise ArgumentError, 'Unsupported selector'
+      end
+    end
+
   end
 
   #----------------------------------------------------------------------#
