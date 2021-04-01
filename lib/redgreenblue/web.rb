84e6fc38f051f2195d16951db94b4b598e1c9f84
lllisteu <lllisteu@users.noreply.github.com>
2021-04-01 08:24:41 +0200

    Add #wcag20_luminance, #wcag20_contrast_ratio.

--- lib/redgreenblue/web.rb
+++ lib/redgreenblue/web.rb
@@ -62,4 +62,41 @@ class RGB
     "##{hex true}"
   end
 
+  # Returns the color's relative luminance, as defined by the Web Content Accessibility Guidelines (WCAG) 2.0.
+  #
+  # This is different from the Y component of CIE 1931 XYZ.
+  #
+  # Based on:
+  # - https://www.w3.org/TR/WCAG20/#relativeluminancedef
+  def wcag20_luminance(round: true)
+    if color_space == 'sRGB'
+      values.map { |v|
+        if v <= 0.03928
+          v / 12.92
+        else
+          ( ( v + 0.055 ) / 1.055 ) ** 2.4
+        end
+      }.zip( [0.2126, 0.7152, 0.0722] ).map { |c,f|
+        c * f
+      }.inject(:+).instance_eval { |v| round ? v.round(8) : v }
+    else
+      raise "can not calculate luminance for color space '#{color_space}'"
+    end
+  end
+
+  # Returns the contrast ratio for this color and another color, as defined by the Web Content Accessibility Guidelines (WCAG) 2.0.
+  #
+  # Based on:
+  # - https://www.w3.org/TR/WCAG20/#contrast-ratiodef
+  def wcag20_contrast_ratio(another, round: true)
+    luminances = [
+      wcag20_luminance(round: false),
+      another.wcag20_luminance(round: false)
+    ].sort.reverse
+
+    contrast_ratio = ( luminances[0] + 0.05 ) / ( luminances[1] + 0.05 )
+
+    round ? contrast_ratio.round(8) : contrast_ratio
+  end
+
 end
--- test/tests/test_web.rb
+++ test/tests/test_web.rb
@@ -19,4 +19,24 @@ class Test_web < Test::Unit::TestCase
     assert_nil   RGB.css 'this is not a color name'
   end
 
+  def test_wcag20_luminance
+    assert_equal 0, RGB.black.wcag20_luminance
+    assert_equal 1, RGB.white.wcag20_luminance
+  end
+
+  def test_wcag20_contrast_ratio
+    # Test values generated with the WebAIM Contrast Checker,
+    # which truncates (not rounds), results to 2 decimals.
+    # See https://webaim.org/resources/contrastchecker/
+    assert_equal 10.68, RGB.hex('ff99cc').wcag20_contrast_ratio(RGB.hex('000000')).floor(2)
+    assert_equal  8   , RGB.hex('0000ff').wcag20_contrast_ratio(RGB.hex('ffff00')).floor(2)
+    assert_equal 21   , RGB.hex('ffffff').wcag20_contrast_ratio(RGB.hex('000000')).floor(2)
+    assert_equal  1   , RGB.hex('ffbb00').wcag20_contrast_ratio(RGB.hex('ffbc00')).floor(2)
+
+    # Order does not matter (does not distinguish foreground/background).
+    a = RGB.hex '123456'
+    b = RGB.hex 'fedcba'
+    assert_equal a.wcag20_contrast_ratio(b), b.wcag20_contrast_ratio(a)
+  end
+
 end
