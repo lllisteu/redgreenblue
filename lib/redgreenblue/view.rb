fbce290934f5d2f5970c20ee002d68aea077fd89
lllisteu <lllisteu@users.noreply.github.com>
2021-06-08 10:39:25 +0200

    #view takes 2nd color.

--- lib/redgreenblue/view.rb
+++ lib/redgreenblue/view.rb
@@ -1,18 +1,24 @@
 class RGB
 
-  # Prints details for the RGB object, using multiple lines.
-  def view
-    puts _inspect_view
+  # Prints a color swatch and details for the RGB object, using multiple lines.
+  #
+  # You can optionally supply a second color to be shown inside the swatch, for comparison.
+  def view(other=nil)
+    puts _inspect_view other
   end
 
   alias v view
 
   private
 
-  def _inspect_view
+  def _inspect_view(other=nil)
     o = []
     o << 3.times.map { terminal_background + "      \e[0m "}
     o << [ "#{_inspect_hex} ", '%-7.7s ' % color_space, '        ' ]
+
+    if other
+      o[0][1] = terminal_background + '  ' + other.terminal_background + '  ' + terminal_background + "  \e[0m "
+    end
 
     o << components.map { |c| c.terminal_background + "  \e[0m " }
     o << %w(R: G: B:)
