2a91ec11b410cb287848374b3917f93db253adfd
lllisteu <lllisteu@users.noreply.github.com>
2021-10-26 14:29:09 +0200

    Expand tests

--- test/tests/test_match.rb
+++ test/tests/test_match.rb
@@ -6,14 +6,17 @@ class Test_match < Test::Unit::TestCase
 
   def test_match_distance
     assert_equal RGB.hex('ffb6c1'), RGB.hex('f9c').match_distance(RGB.css).first.first
+    assert_equal RGB.yellow, RGB.hex('96ea4c').match_distance(RGB.corners).first.first
   end
 
   def test_match_de76
     assert_equal RGB.hex('dda0dd'), RGB.hex('f9c').match_de76(RGB.css).first.first
+    assert_equal RGB.green, RGB.hex('96ea4c').match_de76(RGB.corners).first.first
   end
 
   def test_match_de94
     assert_equal RGB.white, RGB.hex('f9c').match_de94(RGB.corners).first.first
+    assert_equal RGB.green, RGB.hex('96ea4c').match_de94(RGB.corners).first.first
   end
 
 end
