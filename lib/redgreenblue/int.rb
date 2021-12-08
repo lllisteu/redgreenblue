a94b470d58d668c5ef78d837bcb3cd5b454b464c
lllisteu <lllisteu@users.noreply.github.com>
2021-12-08 20:06:05 +0100

    Modularize

--- lib/redgreenblue/int.rb
+++ lib/redgreenblue/int.rb
@@ -1,11 +1,11 @@
-class RGB
+class RGB::Color
 
   # Returns the color as a 24-bit integer in the range 0..16777215.
   def to_i
     ( r << 16 ) + ( g << 8 ) + b
   end
 
-  # Creates a new RGB object from a 24-bit integer in the range 0..16777215.
+  # Creates a new RGB::Color object from a 24-bit integer in the range 0..16777215.
   def self.at(number)
     n = number.to_i
     if (0..16777215) === n
@@ -17,6 +17,23 @@ class RGB
     else
       raise ArgumentError, "Argument '#{number}' not in range 0..16777215"
     end
+  end
+
+end
+
+#----------------------------------------------------------------------#
+#                            Module Methods                            #
+#----------------------------------------------------------------------#
+
+module RGB
+
+  class << self
+
+    # Creates a new RGB::Color object from a 24-bit integer in the range 0..16777215.
+    def at(number)
+      Color.at(number)
+    end
+
   end
 
 end
