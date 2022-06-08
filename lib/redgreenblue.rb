800d74f8561cdf6dddde66342d94b1bf88a72dff
lllisteu <lllisteu@users.noreply.github.com>
2022-06-08 10:09:43 +0200

    Improve docs

--- lib/redgreenblue.rb
+++ lib/redgreenblue.rb
@@ -1,4 +1,23 @@
+# The RGB module is the main namespace for redgreenblue.
+#
+# This module has many helper methods for creating RGB::Color objects.
+# The example below shows seven ways to create an RGB::Color object for the exact same color.
+# @example
+#  require 'redgreenblue'
+#
+#  pink = RGB.new(1, 0.6, 0.8)
+#  pink = RGB.rgb(255, 153, 204)
+#  pink = RGB.rrggbb(65535, 39321, 52428)
+#  pink = RGB.hex('f9c')
+#  pink = RGB.hsl(330, 1, 0.8)
+#  pink = RGB.hsv(330, 0.4, 1)
+#  pink = RGB.at(16751052)
 module RGB
+
+  # This class represents an RGB color.
+  class Color
+  end
+
 end
 
 %w(
