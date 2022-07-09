a1c37b48389b0d5566185a61f97520feca519f10
lllisteu <lllisteu@users.noreply.github.com>
2022-07-09 10:48:56 +0200

    Docs

--- lib/redgreenblue/cie_1931.rb
+++ lib/redgreenblue/cie_1931.rb
@@ -26,11 +26,9 @@ class RGB::Color
     r, g, b = linear_values
 
     [
-
       r * 0.4124_5643_9090 + g * 0.3575_7607_7644 + b * 0.1804_3748_3266,
       r * 0.2126_7285_1406 + g * 0.7151_5215_5288 + b * 0.0721_7499_3307,
       r * 0.0193_3389_5582 + g * 0.1191_9202_5881 + b * 0.9503_0407_8536
-
     ].map { |v| round ? v.round(8) : v }
   end
 
@@ -39,6 +37,11 @@ class RGB::Color
   # Sets the red, green, and blue values by converting the given CIE 1931 XYZ values to RGB.
   #
   # Assumes the XYZ values are relative to D65 reference white, the same as used in sRGB.
+  #
+  # Based on:
+  # - http://www.brucelindbloom.com/index.html?Eqn_XYZ_to_RGB.html
+  # XYZ to sRGB matrix for D65 reference white calculated with Javascript by Bruce Lindbloom:
+  # - http://www.brucelindbloom.com/ColorCalculator.html
   def cie_xyz=(*a)
     x, y, z = a.flatten
     self.linear_values = [
