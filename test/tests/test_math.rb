b25a3ffda239457a74f3e956f7d240825b094dca
lllisteu <lllisteu@users.noreply.github.com>
2022-11-21 14:56:44 +0100

    Add test

--- test/tests/test_math.rb
+++ test/tests/test_math.rb
@@ -4,7 +4,9 @@ require 'redgreenblue'
 
 class Test_math < Test::Unit::TestCase
 
-  def test_simple
+  def test_zip_add
+    assert_equal [100, -5, 3.6], RGB.new.send('zip_add', [90 , 0, 3.6], [10, -5.0, 0])
+    assert_equal [10 , -5, 3.6], RGB.new.send('zip_add', [nil, 0, 3.6], [10, -5.0, 0])
   end
 
 end
