97e3693d57a66bae34f051e72b2cc64fa6a1a1ea
lllisteu <lllisteu@users.noreply.github.com>
2022-11-21 14:59:14 +0100

    Add angle helper

--- lib/redgreenblue/math.rb
+++ lib/redgreenblue/math.rb
@@ -2,6 +2,17 @@ class RGB::Color
 
 private
 
+  # Returns shortest angle of travel (rotation)
+  # to move between 2 points on a circle.
+  #
+  # Some discussions here:
+  # - https://stackoverflow.com/questions/9505862/
+  # - https://stackoverflow.com/questions/7428718/
+  def angle_of_travel(source, destination)
+    angle = destination - source
+    (angle.abs > 180) ? (angle + (angle.negative? ? 360 : -360)) : angle
+  end
+
   def zip_add(a,b)
     a.zip(b).map { |ab| ( ab[0] || 0 ) + ab[1] }
   end
--- test/tests/test_math.rb
+++ test/tests/test_math.rb
@@ -9,4 +9,17 @@ class Test_math < Test::Unit::TestCase
     assert_equal [10 , -5, 3.6], RGB.new.send('zip_add', [nil, 0, 3.6], [10, -5.0, 0])
   end
 
+  def test_angle_of_travel
+    c = RGB.new
+    [
+      [   0,  340,  340],
+      [ 180,    0,  180],
+      [-179,    0,  181],
+      [  40,   10,   50],
+      [ -40,   50,   10],
+    ].each do |a,s,d|
+      assert_equal a, c.send('angle_of_travel', s, d)
+    end
+  end
+
 end
